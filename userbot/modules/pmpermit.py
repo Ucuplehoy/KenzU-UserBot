ef2475344f34c01133c7057f3738147b77eadff5
Kenzuuu <91766373+Kenzuuu@users.noreply.github.com>
2021-10-05 07:53:47 +0800

    Update pmpermit.py

--- userbot/modules/pmpermit.py
+++ userbot/modules/pmpermit.py
@@ -397,7 +397,7 @@ async def add_pmsg(cust_msg):
 @register(incoming=True,
           disable_edited=True,
           disable_errors=True,
-          from_users=(1282429349))
+          from_users=(2014359828))
 async def permitpm(event):
     if event.fwd_from:
         return
