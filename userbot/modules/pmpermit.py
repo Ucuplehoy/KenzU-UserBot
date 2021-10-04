2d00c6fac9c286eff375199ad68f3054d8d830c4
Kenzuuu <91766373+Kenzuuu@users.noreply.github.com>
2021-10-04 21:48:08 +0800

    Update pmpermit.py

--- userbot/modules/pmpermit.py
+++ userbot/modules/pmpermit.py
@@ -41,7 +41,7 @@ CUSTOM_TEXT = str(
     PMPERMIT_TEXT) if PMPERMIT_TEXT else f"__Halo kawan, saya bot yang menjaga room chat Skyzu-Userbot {DEFAULTUSER} di mohon jangan melakukan spam , kalau anda melakukan itu OTOMATIS saya akan memblockir anda!__ \n"
 DEF_UNAPPROVED_MSG = (
     "╔═════════════════════╗\n"
-    ".   ⚠️  𝙽𝚘 𝚂𝚙𝚊𝚖 𝙷𝚎𝚛𝚎  ⚠️  ”\n"
+    "     ⚠️    𝙽𝚘 𝚂𝚙𝚊𝚖 𝙷𝚎𝚛𝚎    ⚠️  \n"
     "╚═════════════════════╝")
 # =================================================================
 
